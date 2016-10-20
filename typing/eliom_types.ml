8a85bffdff43ca0837c14aee242904b988853704
Drup <drupyog@zoho.com>
2016-10-20 18:57:00 +0200

    Add the lifting operation on module types.
    Take a ground module and lift it to the given side, refreshing idents if necessary.

--- typing/eliom_types.ml
+++ typing/eliom_types.ml
@@ -61,3 +61,62 @@ let global_side s =
   !r
 
 let is_mixed s = global_side s = `Shared
+
+
+
+(** Signature lifting
+
+    When a base moduled is used in a client or server scope, we
+    lift the declaration to the appropriate side.
+
+    This allow to type pieces of code such as :
+
+    module%client M = ...
+    module%client S = Set.Make(M)
+*)
+module Lift = struct
+
+  let refresh ~side id = match side, Ident.side id with
+    | (`Client | `Server | `Shared), `Noside ->
+        Ident.rename ~side id
+    | _ -> id
+
+  let rec signature_component side = function
+    | Sig_value(id, d) ->
+        Sig_value(refresh ~side id, d)
+    | Sig_type(id, d, rs) ->
+        Sig_type(refresh ~side id, d, rs)
+    | Sig_typext(id, ext, es) ->
+        Sig_typext(refresh ~side id, ext, es)
+    | Sig_module(id, d, rs) ->
+        Sig_module(refresh ~side id, module_decl side d, rs)
+    | Sig_modtype(id, d) ->
+        Sig_modtype(refresh ~side id, modtype_decl side d)
+    | Sig_class(id, d, rs) ->
+        Sig_class(refresh ~side id, d, rs)
+    | Sig_class_type(id, d, rs) ->
+        Sig_class_type(refresh ~side id, d, rs)
+
+  and module_decl side md =
+    { md with md_type = module_type side md.md_type }
+  and modtype_decl side mtd =
+    { mtd with mtd_type = Misc.may_map (module_type side) mtd.mtd_type }
+
+  and signature side x = List.map (signature_component side) x
+
+  and module_type side = function
+    | Mty_signature s -> Mty_signature (signature side s)
+    | Mty_functor (id, param, res) ->
+        let id' = refresh ~side id in
+        let param' = Misc.may_map (module_type side) param in
+        let res =
+          if Ident.same id' id then res
+          else Subst.(modtype (add_module id (Path.Pident id') identity)) res
+        in
+        let res' = module_type side res in
+        Mty_functor (id', param', res')
+
+    | Mty_ident _ | Mty_alias _ as mty -> mty
+
+
+end
--- typing/eliom_types.mli
+++ typing/eliom_types.mli
@@ -5,3 +5,12 @@ val cmi_magic_number : string
 val translate : [< Eliom_base.shside] -> Types.signature -> unit
 
 val is_mixed : Types.signature -> bool
+
+module Lift : sig
+  val modtype_decl :
+    Eliom_base.shside -> Types.modtype_declaration -> Types.modtype_declaration
+  val module_decl :
+    Eliom_base.shside -> Types.module_declaration -> Types.module_declaration
+  val module_type :
+    Eliom_base.shside -> Types.module_type -> Types.module_type
+end
